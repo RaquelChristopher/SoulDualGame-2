3a553da4d338b84e8110311fa47f02ca2dd309d5
Jr Christopher <jrtt212@gmail.com>
2025-07-19 10:31:32 +1000

    Initial Commit

---
+++ Soul Dual Game 2/Soul_Dual_Game_2App.swift
@@ -0,0 +1,17 @@
+//
+//  Soul_Dual_Game_2App.swift
+//  Soul Dual Game 2
+//
+//  Created by Jedda Tuuta on 19/7/2025.
+//
+
+import SwiftUI
+
+@main
+struct Soul_Dual_Game_2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
